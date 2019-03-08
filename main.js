3b8c07e5ced533fae5f26915495d52a8c617c1aa
krishnansuccess <krishnan.success21@gmail.com>
2019-03-08 18:11:28 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-require(['jquery', 'jquery.bootstrap'], function($){
-    // DOM ready
-    $(function(){
-        // This function is needed (even if empty) to force RequireJS to load Twitter Bootstrap and its Data API.
-    });
-});
